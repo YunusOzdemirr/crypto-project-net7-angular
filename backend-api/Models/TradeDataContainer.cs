304a43dbf2bbe11d02508aa2a4b1fade9a26c05e
yunusozdemirr <yunusozdemir468@gmail.com>
2025-07-02 23:59:01 +0300

    Code refactor

--- backend-api/Models/TradeDataContainer.cs
+++ backend-api/Models/TradeDataContainer.cs
@@ -1,3 +1,6 @@
+using Binance.Net.Interfaces;
+using Binance.Net.Objects.Models.Spot;
+
 namespace crypto_api.Models;
 
 public class TradeDataContainer
@@ -34,4 +37,57 @@ public class TradeDataContainer
     public List<TradeOrderAsk> TradeOrderAsks { get; set; } = new List<TradeOrderAsk>();
     public List<TradeOrderBid> TradeOrderBids { get; set; } = new List<TradeOrderBid>();
 
+    public TradeDataContainer()
+    {
+
+    }
+    public TradeDataContainer(int symbolIndex, decimal olddataprice, BinanceProduct symbol, IBinance24HPrice coin, TradeDataContainer oldData)
+    {
+        decimal targetLevel = 0, stopLevel = 0;
+        int targetCount = 0, stopCount = 0;
+        if (oldData is not null)
+        {
+            targetLevel = oldData.TargetLevel <= coin.LastPrice
+                               ? coin.LastPrice * (decimal)1.03
+                               : oldData.TargetLevel;
+            stopLevel = oldData.StopLevel >= coin.LastPrice
+                           ? coin.LastPrice * (decimal)0.97
+                           : oldData.StopLevel;
+            targetCount = oldData.TargetLevel <= coin.LastPrice ? +1 : oldData.TargetCount;
+            stopCount = oldData.StopLevel <= coin.LastPrice ? +1 : oldData.StopCount;
+        }
+        else
+        {
+            targetLevel = coin.LastPrice * (decimal)1.03;
+            stopLevel = coin.LastPrice * (decimal)0.97;
+        }
+        Id = symbolIndex;
+        Symbol = coin.Symbol;
+        PriceChange = coin.PriceChange;
+        PriceChangePercent = coin.PriceChangePercent;
+        Volume = coin.Volume;
+        OpenPrice = coin.OpenPrice;
+        HighPrice = coin.HighPrice;
+        LowPrice = coin.LowPrice;
+        ClosePrice = symbol.ClosePrice ?? 0m;
+        LastPrice = coin.LastPrice;
+        LastPrice2 = olddataprice != 0 ? olddataprice : 0;
+        BaseAsset = symbol.BaseAsset;
+        BaseAssetName = symbol.BaseAssetName;
+        QuoteAsset = symbol.QuoteAsset;
+        QuoteAssetName = symbol.QuoteAssetName;
+        PrevDayClosePrice = coin.PriceChange;
+        QuoteVolume = coin.QuoteVolume;
+        WeightedAveragePrice = coin.WeightedAveragePrice;
+        TotalTrades = coin.TotalTrades;
+        CirculatingSupply = symbol.CirculatingSupply != null
+                            ? (decimal)symbol.CirculatingSupply
+                            : (decimal)0;
+        TargetLevel = targetLevel;
+        StopLevel = stopLevel;
+        TargetCount = targetCount;
+        StopCount = stopCount;
+        TradeOrderBids = oldData?.TradeOrderBids;
+        TradeOrderAsks = oldData?.TradeOrderAsks;
+    }
 }
--- backend-api/Services/BinanceSocketService.cs
+++ backend-api/Services/BinanceSocketService.cs
@@ -1,12 +1,11 @@
-using System.Collections.Concurrent;
-using System.Reactive.Subjects;
-using System.Runtime.CompilerServices;
 using Binance.Net.Interfaces;
-using Binance.Net.Objects.Models.Futures;
 using Binance.Net.Objects.Models.Spot;
 using crypto_api.Interfaces;
 using crypto_api.Models;
 using CryptoExchange.Net.Sockets;
+using System.Collections.Concurrent;
+using System.Reactive.Subjects;
+using System.Runtime.CompilerServices;
 
 namespace crypto_api.Services;
 
@@ -17,8 +16,8 @@ public class BinanceSocketService : IBinanceSocketService
     public static ConcurrentDictionary<string, decimal> OldDataDictionary;
     private IBinanceService _binanceService;
     public static List<BinanceProduct> _symbols;
-    static int delay = 750;
-    public static int sayac = 0;
+    public const int delay = 750;
+    public int counter = 0;
     public readonly Subject<TradeDataContainer> _subject = new();
 
     public BinanceSocketService(IBinanceService binanceService)
@@ -28,6 +27,20 @@ public class BinanceSocketService : IBinanceSocketService
         TickerDictionary = new ConcurrentDictionary<string, TradeDataContainer>();
         _binanceService = binanceService;
         _symbols = _binanceService.GetProductsAsync().Result as List<BinanceProduct>;
+        if (_symbols is null)
+        {
+            _symbols = new List<BinanceProduct>()
+            {
+                new BinanceProduct()
+                {
+                    Symbol = "BTCUSDT",
+                    BaseAsset = "BTC",
+                    QuoteAsset = "USDT",
+                    BaseAssetName = "Bitcoin",
+                    QuoteAssetName = "Tether"
+                }
+            };
+        }
         Thread backgroundThread = new Thread(new ThreadStart(Start));
         backgroundThread.Start();
     }
@@ -49,28 +62,25 @@ public class BinanceSocketService : IBinanceSocketService
     public async IAsyncEnumerable<object> GetDetailAsync(string symbol,
         [EnumeratorCancellation] CancellationToken cancellationToken)
     {
-        if (TickerDictionary.TryGetValue(symbol, out TradeDataContainer _))
-            while (true)
-            {
-                if (cancellationToken.IsCancellationRequested)
-                    cancellationToken.ThrowIfCancellationRequested();
-                TickerDictionary.TryGetValue(symbol, out TradeDataContainer data);
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            TickerDictionary.TryGetValue(symbol, out TradeDataContainer data);
 
-                TickerDictionaryHistory.TryGetValue(symbol, out var chart);
-                if (chart is null)
-                    chart = new List<KlineDataContainer>
+            TickerDictionaryHistory.TryGetValue(symbol, out var chart);
+            if (chart is null)
+                chart = new List<KlineDataContainer>
                         { new KlineDataContainer { LastPrice = data.LastPrice, TimeStamp = DateTime.Now } };
-                yield return new { Token = data, Chart = chart };
-                await Task.Delay(delay);
-            }
+            yield return new { Token = data, Chart = chart };
+            await Task.Delay(delay);
+        }
     }
 
     public async IAsyncEnumerable<object> DataStream([EnumeratorCancellation] CancellationToken cancellationToken)
     {
-        while (true)
+        while (!cancellationToken.IsCancellationRequested)
         {
-            if (cancellationToken.IsCancellationRequested)
-                cancellationToken.ThrowIfCancellationRequested();
+            cancellationToken.ThrowIfCancellationRequested();
             foreach (var coin in TickerDictionary)
             {
                 TickerDictionaryHistory.TryGetValue(coin.Key, out var chart);
@@ -93,125 +103,63 @@ public class BinanceSocketService : IBinanceSocketService
                 ? _symbols.Find(a => a.Symbol == coin.Symbol)
                 : null;
 
-            if (symbol is not null)
+            if (symbol is null)
+                continue;
+            OldDataDictionary.TryGetValue(coin.Symbol, out var olddataprice);
+
+            if (TickerDictionary.TryGetValue(coin.Symbol, out TradeDataContainer oldData))
             {
-                OldDataDictionary.TryGetValue(coin.Symbol, out var olddataprice);
-
-                if (TickerDictionary.TryGetValue(coin.Symbol, out TradeDataContainer oldData))
-                {
-                    TickerDictionary[coin.Symbol] = new TradeDataContainer
-                    {
-                        Id = _symbols.FindIndex(a => a.Symbol == coin.Symbol),
-                        Symbol = coin.Symbol,
-                        PriceChange = coin.PriceChange,
-                        PriceChangePercent = coin.PriceChangePercent,
-                        Volume = coin.Volume,
-                        OpenPrice = coin.OpenPrice,
-                        HighPrice = coin.HighPrice,
-                        LowPrice = coin.LowPrice,
-                        ClosePrice = (decimal)symbol.ClosePrice,
-                        LastPrice = coin.LastPrice,
-                        LastPrice2 = olddataprice != 0 ? olddataprice : 0,
-                        BaseAsset = symbol.BaseAsset,
-                        BaseAssetName = symbol.BaseAssetName,
-                        QuoteAsset = symbol.QuoteAsset,
-                        QuoteAssetName = symbol.QuoteAssetName,
-                        PrevDayClosePrice = coin.PriceChange,
-                        QuoteVolume = coin.QuoteVolume,
-                        WeightedAveragePrice = coin.WeightedAveragePrice,
-                        TotalTrades = coin.TotalTrades,
-                        CirculatingSupply = symbol.CirculatingSupply != null
-                            ? (decimal)symbol.CirculatingSupply
-                            : (decimal)0,
-                        TargetLevel = oldData.TargetLevel <= coin.LastPrice
-                            ? coin.LastPrice * (decimal)1.03
-                            : oldData.TargetLevel,
-                        StopLevel = oldData.StopLevel >= coin.LastPrice
-                            ? coin.LastPrice * (decimal)0.97
-                            : oldData.StopLevel,
-                        TargetCount = oldData.TargetLevel <= coin.LastPrice ? +1 : oldData.TargetCount,
-                        StopCount = oldData.StopLevel <= coin.LastPrice ? +1 : oldData.StopCount,
-                        TradeOrderBids = oldData.TradeOrderBids,
-                        TradeOrderAsks = oldData.TradeOrderAsks
-                    };
-                    _subject.OnNext(TickerDictionary[coin.Symbol]);
-                }
-                else
-                {
-                    TickerDictionary.TryAdd(coin.Symbol, new TradeDataContainer
-                    {
-                        Id = _symbols.FindIndex(a => a.Symbol == coin.Symbol),
-                        Symbol = coin.Symbol,
-                        PriceChange = coin.PriceChange,
-                        PriceChangePercent = coin.PriceChangePercent,
-                        Volume = coin.Volume,
-                        OpenPrice = coin.OpenPrice,
-                        HighPrice = coin.HighPrice,
-                        LowPrice = coin.LowPrice,
-                        ClosePrice = (decimal)symbol.ClosePrice,
-                        LastPrice = coin.LastPrice,
-                        LastPrice2 = olddataprice != 0 ? olddataprice : 0,
-                        BaseAsset = symbol.BaseAsset,
-                        BaseAssetName = symbol.BaseAssetName,
-                        QuoteAsset = symbol.QuoteAsset,
-                        QuoteAssetName = symbol.QuoteAssetName,
-                        PrevDayClosePrice = coin.PriceChange,
-                        QuoteVolume = coin.QuoteVolume,
-                        WeightedAveragePrice = coin.WeightedAveragePrice,
-                        TotalTrades = coin.TotalTrades,
-                        CirculatingSupply = symbol.CirculatingSupply != null
-                            ? (decimal)symbol.CirculatingSupply
-                            : (decimal)0,
-                        TargetLevel = coin.LastPrice * (decimal)1.03,
-                        StopLevel = coin.LastPrice * (decimal)0.97,
-                        TargetCount = 0,
-                        StopCount = 0
-                    });
-                    _subject.OnNext(TickerDictionary[coin.Symbol]);
-                }
-
-                if (sayac == 300)
-                    OldDataDictionary.TryAdd(coin.Symbol, coin.LastPrice);
-
-                sayac++;
-
-                if (TickerDictionaryHistory.TryGetValue(coin.Symbol, out var existingItem))
-                {
-                    var lastItem = existingItem.Last();
-                    if (lastItem is null)
-                    {
-                        existingItem.RemoveAt(existingItem.Count - 1);
-                        lastItem = existingItem.Last();
-                    }
-
-                    if (existingItem.Count() >= 30 && (DateTime.Now.Second - lastItem.TimeStamp.Second >= 6 ||
-                                                       DateTime.Now.Minute - lastItem.TimeStamp.Minute >= 1))
-                        existingItem.RemoveAt(0);
-                    if (lastItem.LastPrice != coin.LastPrice &&
-                        (DateTime.Now.Second - lastItem.TimeStamp.Second >= 6 ||
-                         DateTime.Now.Minute - lastItem.TimeStamp.Minute >= 1))
-                        existingItem.Add(new KlineDataContainer
-                        {
-                            LastPrice = coin.LastPrice,
-                            TimeStamp = DateTime.Now
-                        });
-                }
-                else
-                    TickerDictionaryHistory.TryAdd(coin.Symbol, new List<KlineDataContainer>()
-                    {
-                        new KlineDataContainer
-                        {
-                            LastPrice = coin.LastPrice,
-                            TimeStamp = DateTime.Now
-                        }
-                    });
-
-                //var ticker = TickerDictionaryHistory[coin.Symbol];
-                //if (ticker.IsEmpty) return;
-                //var copy = new List<DateTime>(ticker.Keys);
-                //copy.Where(x => (dataEvent.Timestamp - x).TotalSeconds >= 60).ToList().ForEach(x => ticker.TryRemove(x, out _));
-                await Task.Delay(750);
+                TickerDictionary[coin.Symbol] = new TradeDataContainer(oldData.Id, olddataprice, symbol, coin, oldData);
+                _subject.OnNext(TickerDictionary[coin.Symbol]);
             }
+            else
+            {
+                int symbolIndex = _symbols.FindIndex(a => a.Symbol == coin.Symbol);
+                TickerDictionary.TryAdd(coin.Symbol, new TradeDataContainer(symbolIndex, olddataprice, symbol, coin, null));
+                _subject.OnNext(TickerDictionary[coin.Symbol]);
+            }
+
+            if (counter == 300)
+                OldDataDictionary.TryAdd(coin.Symbol, coin.LastPrice);
+
+            counter++;
+
+            if (TickerDictionaryHistory.TryGetValue(coin.Symbol, out var existingItem))
+            {
+                var lastItem = existingItem.Last();
+                if (lastItem is null)
+                {
+                    existingItem.RemoveAt(existingItem.Count - 1);
+                    lastItem = existingItem.Last();
+                }
+
+                if (existingItem.Count() >= 30 && (DateTime.Now.Second - lastItem.TimeStamp.Second >= 6 ||
+                                                   DateTime.Now.Minute - lastItem.TimeStamp.Minute >= 1))
+                    existingItem.RemoveAt(0);
+                if (lastItem.LastPrice != coin.LastPrice &&
+                    (DateTime.Now.Second - lastItem.TimeStamp.Second >= 6 ||
+                     DateTime.Now.Minute - lastItem.TimeStamp.Minute >= 1))
+                    existingItem.Add(new KlineDataContainer
+                    {
+                        LastPrice = coin.LastPrice,
+                        TimeStamp = DateTime.Now
+                    });
+            }
+            else
+                TickerDictionaryHistory.TryAdd(coin.Symbol, new List<KlineDataContainer>()
+                {
+                    new KlineDataContainer
+                    {
+                        LastPrice = coin.LastPrice,
+                        TimeStamp = DateTime.Now
+                    }
+                });
+
+            //var ticker = TickerDictionaryHistory[coin.Symbol];
+            //if (ticker.IsEmpty) return;
+            //var copy = new List<DateTime>(ticker.Keys);
+            //copy.Where(x => (dataEvent.Timestamp - x).TotalSeconds >= 60).ToList().ForEach(x => ticker.TryRemove(x, out _));
+            await Task.Delay(750);
         }
     }
 }
